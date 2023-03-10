2a5de749df68b7d82355b344204efae2484654b5
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-10 19:38:16 +0000

    dataset contains encodings length

--- src/mlp/dataset.h
+++ src/mlp/dataset.h
@@ -15,14 +15,19 @@ namespace mlp {
 		Set train, validate, test;
 
 		Dataset(std::filesystem::path path) {
-			std::ifstream file{path, std::ifstream::in | std::ifstream::binary};
-			if (file.is_open()) {
+			std::ifstream file;
+			file.exceptions(std::ios::failbit | std::ios::badbit);
+			try {
+				file.open(path, std::ifstream::in | std::ifstream::binary);
 				Set* sets[] = { &train, &validate, &test };
 				for (auto set : sets) {
 					size_t size;
 					file.read((char*)&size, sizeof(size));
 					set->resize(size);
 				}
+				size_t encoding_size;
+				file.read((char*)&encoding_size, sizeof(encoding_size));
+				mlp_assert(encoding_size == encodings.size(), "Number of Encodings mismatches the number of columns in the dataset!");
 
 				struct EncodingRaw {
 					FLOAT min, max;
@@ -35,12 +40,26 @@ namespace mlp {
 
 				for (auto set : sets)
 					file.read((char*)set->data(), sizeof(Set::value_type) * set->size());
+
+				file.close();
+				success = true;
+			}
+			catch (std::ios::failure& e) {
+				mlp_log_fatal("Dataset [{}] could not be opened: {}", path.generic_string(), e.what());
+			}
+			catch (std::exception& e) {
+				mlp_log_fatal("Dataset [{}] failed reading: ", path.generic_string(), e.what());
 			}
 		}
 
 		NODISCARD FLOAT decode_output(FLOAT value) const {
 			return encodings.back().decode(value);
 		}
+
+	protected:
+		bool success = false;
+	public:
+		NODISCARD operator bool() const { return success; }
 	};
 
 }  // namespace mlp
