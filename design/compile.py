608ea57ff1f410411600b1c317565e27556a5db0
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-15 16:34:47 +0000

    report pdf setup

---
+++ design/compile.py
@@ -0,0 +1,48 @@
+# pandoc report.md -o report.pdf --pdf-engine-opt=-shell-escape
+
+import subprocess
+import sys
+from pathlib import Path
+
+directory = Path.cwd()
+
+markdown_files = [(f := directory / "design" / i, directory / "design" / "output" / f"{f.stem}.pdf")
+                  for i in ["report.md", "code.md"]]
+
+
+def files(*exts: str) -> list[Path]:
+    src = directory / "src"
+    files: list[Path] = []
+    for ext in exts:
+        for p in src.rglob(f"*{ext}"):
+            if p.is_file():
+                files.append(p)
+    return files
+
+
+def f2latex(files: list[Path]) -> str:
+    return ", ".join(f".{str(i)[len(str(directory)):]}" for i in files).replace("\\", "/")
+
+
+MAPPINGS = {
+    "PYTHON_FILES": f2latex(files(".py")),
+    "CPP_FILES": f2latex(files(".cpp", ".h")),
+}
+
+print(f"Compiling: {len(markdown_files)}")
+for input, output in markdown_files:
+    print(f"\t{input}")
+    with open(input, "r") as file:
+        content = file.read()
+        for k, v in MAPPINGS.items():
+            content = content.replace(f"PY_{k}", v)
+    proc = subprocess.run(
+        f'pandoc -o "{output}" --pdf-engine-opt=-shell-escape', input=content.encode("utf8"), capture_output=True, cwd=str(directory))
+
+    try:
+        proc.check_returncode()
+    except Exception:
+        print(f"{proc.stderr.decode('utf8')}")
+        raise
+
+print("Done!")
---
+++ design/report.md
@@ -0,0 +1,29 @@
+---
+title: Multi-Layer Perceptron
+subtitle: Report
+author: F121584 - Thomas Woolhouse
+date: 22/03/2023
+numbersections: false
+documentclass: report
+papersize: A4
+fontsize: 11pt
+geometry: margin=2cm
+toc: true
+links-as-notes: true
+colorlinks: blue
+header-includes: |
+	\usepackage{sectsty}
+	\sectionfont{\clearpage}
+	\usepackage{pgffor}
+	\usepackage{minted}
+---
+
+# Data Preprocessing
+
+# Implementation of MLP
+
+# Training and Network Selection
+
+# Evaluation of Final Model
+
+# Comparison with Other Models
