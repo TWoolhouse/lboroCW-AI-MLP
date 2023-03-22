0e2d3970a078b2fad42348eddee9e0c4a7f464ee
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-22 15:42:10 +0000

    variant sets

--- src/process/variants.py
+++ src/process/variants.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from re import A
+from typing import Literal, Optional
 
 import activation
 import filter as filter_
@@ -13,19 +14,41 @@ def variant_name(*variants: Optional[str]) -> str:
     return ".".join(filter(None, variants))
 
 
-def all():
-    return filter_.VARIANTS, standardise.VARIANTS, split.VARIANTS
-
-
 def datasets():
     return [(i, j, k) for i in filter_.VARIANTS.items() for j in standardise.VARIANTS.items() for k in split.VARIANTS.items()]
 
 
 def builds():
-    return [(i, j, k) for i in height.variants(len(FIELDS)) for j in activation.VARIANTS.items() for k in modification.VARIANTS.items()]
+    return [(i, j, k) for i in height.variants(len(FIELDS) - 1) for j in activation.VARIANTS.items() for k in modification.VARIANTS.items()]
 
 
 def train():
     def vname(var):
         return variant_name(*(v[0] for v in var))
     return [(variant_name(vname(dataset), vname(build)), vname(dataset), vname(build)) for dataset in datasets() for build in builds()]
+
+
+LABELS = [
+    "filter",
+    "standardise",
+    "split",
+    "height",
+    "activation",
+    "modification",
+]
+
+VARIANTS: dict[str, set[str]] = {}
+TYPES: dict[Literal[
+    "filter",
+    "standardise",
+    "split",
+    "height",
+    "activation",
+    "modification",
+], set[str]] = {}
+ALL: set[str] = {n for n, _, _ in train()}
+
+for name in ALL:
+    for i, section in enumerate(name.split(".")):
+        VARIANTS.setdefault(section, set()).add(name)
+        TYPES.setdefault(LABELS[min(i, len(LABELS) - 1)], set()).add(section)
