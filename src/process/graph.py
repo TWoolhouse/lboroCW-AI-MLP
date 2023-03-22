510d2b2c2785b2ce5b91177ec08ca285da02c16a
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-22 15:41:59 +0000

    graph many things

--- src/process/graph.py
+++ src/process/graph.py
@@ -4,14 +4,24 @@ from pathlib import Path
 from typing import Collection
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+import variants
 from matplotlib.axes import Axes
 from record import FIELDS, Record
 from standardise import Encoding
+from variants import variant_name
 
-Path("graph/dataset").resolve().mkdir(parents=True, exist_ok=True)
-Path("graph/model").resolve().mkdir(parents=True, exist_ok=True)
-Path("graph/all").resolve().mkdir(parents=True, exist_ok=True)
+SUBDIRS = [
+    "dataset",
+    "model",
+    "all",
+    "best",
+    "modification",
+    "dataset_group",
+]
+
+for subdir in SUBDIRS:
+    Path(f"graph/{subdir}").resolve().mkdir(parents=True, exist_ok=True)
 
 
 def processed(name: str, encodings: list[Encoding], train: list[Record], validate: list[Record], test: list[Record]):
@@ -33,7 +43,7 @@ def processed(name: str, encodings: list[Encoding], train: list[Record], validat
     ) for rec in ds])
 
 
-COLOURS = ["red", "orange", "yellow", "green", "blue", "purple"]
+COLOURS = ["red", "orange", "green", "blue", "purple", "black"]
 
 
 def dataset(name: str, dataset: list[Record]):
@@ -101,7 +111,7 @@ def model_training(name: str, name_ds: str, name_model: str):
     ] if pred()]
 
     fig, axes = plt.subplots(len(plotting))
-    if not isinstance(axes, numpy.ndarray):
+    if not isinstance(axes, np.ndarray):
         axes = [axes]
 
     for ax, func in zip(axes, plotting):
@@ -113,7 +123,79 @@ def model_training(name: str, name_ds: str, name_model: str):
     plt.close(fig)
 
 
-def model_test_histogram(trainers: list[tuple[str, str, str]]):
+def model_modifcations():
+    prefixes = {".".join([v for i, v in enumerate(variant.split(".")) if i < (len(variants.TYPES) - 1)])
+                for variant in variants.ALL
+                }
+
+    def graph_error(epochs, data, title, suffix, ax, colour):
+        ax.plot(epochs, data, color=colour, label=suffix)
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("RMSE")
+        ax.set_title(title, loc="left")
+
+    for prefix in prefixes:
+        print(f"\t{prefix}")
+        modded = {i for i in variants.ALL if i.startswith(prefix)}
+
+        fig, (ax_train, ax_valid) = plt.subplots(2)
+
+        for mod, colour in zip(modded, cycle(COLOURS)):
+            data = read_log(mod)
+            columns = tuple(zip(*data))
+            epochs, error_train, error_validate, learning_rate = columns[:4]
+            m_name = mod[len(prefix)+1:]
+            if len(m_name.strip()) == 0:
+                m_name = "default"
+            graph_error(epochs, error_train, "Training",
+                        m_name, ax_train, colour)
+            graph_error(epochs, error_validate, "Validation",
+                        None, ax_valid, colour)
+
+        fig.suptitle(prefix)
+        fig.legend(ncols=2, loc="upper right", bbox_to_anchor=(1.0, 0.95))
+        fig.tight_layout(h_pad=0.5)
+        fig.savefig(f"graph/modification/{prefix}.png")
+        plt.close(fig)
+    # results = read_log(name)
+
+
+def model_dataset():
+    builds = {build for _, _, build in variants.train()}
+
+    def graph_error(epochs, data, title, suffix, ax, colour):
+        ax.plot(epochs, data, color=colour, label=suffix)
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("RMSE")
+        ax.set_title(title, loc="left")
+
+    for build in builds:
+        models = variants.ALL.copy()
+        for v in build.split("."):
+            models.intersection_update(variants.VARIANTS[v])
+        models = {i for i in models if i.endswith(build)}
+        print(f"\t{build}")
+
+        fig, (ax_train, ax_valid) = plt.subplots(2)
+
+        for model, colour in zip(models, cycle(COLOURS)):
+            data = read_log(model)
+            columns = tuple(zip(*data))
+            epochs, error_train, error_validate, learning_rate = columns[:4]
+            d_name = model[:-(len(build) + 1)]
+            graph_error(epochs, error_train, "Training",
+                        d_name, ax_train, colour)
+            graph_error(epochs, error_validate, "Validation",
+                        None, ax_valid, colour)
+
+        fig.suptitle(build)
+        fig.legend(ncols=1, loc="upper right", bbox_to_anchor=(1.0, 0.95))
+        fig.tight_layout(h_pad=0.5)
+        fig.savefig(f"graph/dataset_group/{build}.png")
+        plt.close(fig)
+
+
+def model_epochs_taken(trainers: list[tuple[str, str, str]]):
     winners: list[tuple[tuple[str, str, str], tuple[float, ...]]] = []
     for name, dataset, model in trainers:
         results = read_log(name)
@@ -123,7 +205,7 @@ def model_test_histogram(trainers: list[tuple[str, str, str]]):
     epoch_max = int(max(winners, key=lambda x: x[1][0])[1][0])
     non_epoch_max = len([None for m, v in winners if v[0] != epoch_max])
     print(
-        f"\tNumber of Model not requiring the max {epoch_max} epochs: {non_epoch_max}")
+        f"\tNumber of Models not requiring the max {epoch_max} epochs: {non_epoch_max}")
 
     def histogram(ax: Axes):
         ax.hist([v[0] for m, v in winners], bins=20)
@@ -142,7 +224,7 @@ def model_test_histogram(trainers: list[tuple[str, str, str]]):
     def pie(ax: Axes):
         ax.pie([non_epoch_max, len(winners) - non_epoch_max],
                explode=[0.1, 0.1],
-               autopct="%d%%",
+               autopct="%.2f%%",
                radius=2,
                labels=[f"Best after <{epoch_max} epochs",
                        f"Best after {epoch_max} epochs"]
@@ -150,7 +232,7 @@ def model_test_histogram(trainers: list[tuple[str, str, str]]):
 
     funcs = [scatter, pie]
     fig, axes = plt.subplots(len(funcs))
-    if not isinstance(axes, numpy.ndarray):
+    if not isinstance(axes, np.ndarray):
         axes: list[Axes] = [axes]
 
     for ax, func in zip(axes, funcs):
@@ -158,4 +240,77 @@ def model_test_histogram(trainers: list[tuple[str, str, str]]):
 
     fig.tight_layout(h_pad=1)
     fig.savefig("graph/all/best_epochs.png", dpi=400)
+    plt.close(fig)
+
+
+def prediction(name: str, encoding: Encoding, predictions: list[list[tuple[Record, float, float]]]):
+    flat = [r for set in predictions for r in set]
+    preds = sorted(flat, key=lambda x: x[0].date)
+
+    data = [(
+            rec.date,
+            rec.evaporation,
+            guess,
+            encoding.decode(rec.evaporation),
+            val
+            )
+            for rec, guess, val in preds
+            ]
+
+    dates, evap, guess, evap_raw, guess_raw = zip(*data)
+
+    fig, (ax_scatter, ax_line) = plt.subplots(2)
+    fig.set_size_inches(32 / 9 * 4, 2 * 4)
+
+    for (d, n), colour in zip([(guess_raw, "Prediction"), (evap_raw, "Dataset")], cycle(["red", "blue"])):
+        ax_scatter.plot(dates, d, "o", markersize=2, label=n, color=colour)
+    ax_scatter.set_title("Pan Evaporation on a Specific Date")
+    ax_scatter.set_ylabel("Pan Evaporation")
+
+    diff = [g - r for g, r in zip(guess_raw, evap_raw)]
+    for (d, n), colour in zip([(diff, None)], cycle(["red", "blue"])):
+        ax_line.plot(dates, d, "o", markersize=2, label=n, color=colour)
+    ax_line.set_title("Difference between the Actual Value and the Prediction")
+    ax_line.set_xlabel("Date")
+    ax_line.set_ylabel("Error")
+
+    nm = name.replace("/", ".")
+    fig.suptitle(name)
+    fig.legend()
+    fig.savefig(f"graph/best/{nm}.png", dpi=500)
+    plt.close(fig)
+
+    # LINEAR REGRESSION
+
+    y = [rec.evaporation for rec, _, _ in preds]
+    x_r: list[list[float]] = [[getattr(rec, field)
+                               for rec, _, _ in preds] for field in [f for f in FIELDS if f != "evaporation"]]
+
+    x = np.transpose(x_r)
+    x = np.c_[x, np.ones(x.shape[0])]  # add bias term
+    linreg: list[float] = list(np.linalg.lstsq(x, y, rcond=None)[0])
+
+    fig, (ax_scatter, ax_line) = plt.subplots(2)
+    fig.set_size_inches(32 / 9 * 4, 2 * 4)
+
+    linest = [sum([a * b for a, b in zip([*row, 1.0], linreg)])
+              for row in zip(*x_r)]
+
+    for (d, n), colour in zip([(guess_raw, "Prediction"), (evap_raw, "Dataset"), (linest, "Linear Regression")], cycle(["red", "purple", "blue"])):
+        ax_scatter.plot(dates, d, "o", markersize=2, label=n, color=colour)
+    ax_scatter.set_title("Pan Evaporation on a Specific Date")
+    ax_scatter.set_ylabel("Pan Evaporation")
+
+    diff_pred = [g - r for g, r in zip(guess_raw, evap_raw)]
+    diff_linest = [g - r for g, r in zip(linest, evap_raw)]
+    for (d, n), colour in zip([(diff_pred, None), (diff_linest, None)], cycle(["red", "blue"])):
+        ax_line.plot(dates, d, "o", markersize=2, label=n, color=colour)
+    ax_line.set_title("Difference between the Actual Value and the Prediction")
+    ax_line.set_xlabel("Date")
+    ax_line.set_ylabel("Error")
+
+    nm = name.replace("/", ".")
+    fig.suptitle(name)
+    fig.legend()
+    fig.savefig(f"graph/best/{nm}.linreg.png", dpi=500)
     plt.close(fig)
